7845ba13536a101592860e51939cbd4774c7b48f
Artyom Antyipin <artyom.szasa@gmail.com>
2022-03-20 10:01:57 +0100

    json fix.

--- NCoreUtils.Queue.Processor/MediaSerializerContext.cs
+++ NCoreUtils.Queue.Processor/MediaSerializerContext.cs
@@ -2,6 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace NCoreUtils.Queue;
 
+[JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase)]
 [JsonSerializable(typeof(PubSubRequest))]
 [JsonSerializable(typeof(PubSubMessage))]
 public partial class MediaSerializerContext : JsonSerializerContext
